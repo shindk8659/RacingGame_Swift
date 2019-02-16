c9a9e294c3548f8f962b21cf08c96f10d261cd74
신동규 <shindk8659@naver.com>
2019-02-16 17:19:23 +0900

    create CCStudy_RacingCar Project

---
+++ CarRacingGameTests/CarRacingGameTests.swift
@@ -0,0 +1,41 @@
+//
+//  CarRacingGameTests.swift
+//  CarRacingGameTests
+//
+//  Created by 신동규 on 2/16/19.
+//  Copyright © 2019 CCStudy. All rights reserved.
+//
+
+import XCTest
+@testable import CCStudy_RacingCar
+
+class CarRacingGameTests: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testExample() {
+        
+    }
+    
+    func testPerformanceExample() {
+        
+        let expectedValue:Int  = 0
+        var checkValue:Bool = false
+        
+        if CarRacingGame.moveCheck(random: expectedValue) == "-" {
+            checkValue = true
+            
+        }
+        else {
+            checkValue = false
+        }
+        XCTAssertTrue(checkValue, "전진값이 아닙니다.")
+    }
+    
+}
